54099ef76d3691d62b57fee9a081ebe0311382be
White Negra <120759798+negra495@users.noreply.github.com>
2024-08-29 12:13:24 +0300

    Add files via upload

---
+++ carrying.closure/carrying.js
@@ -0,0 +1,79 @@
+// function funk(a,b,c,d){
+//     return a+b+c+d
+// }
+//
+// console.log(funk(10,20,30,40))
+//
+// function carriyng(a){
+//     return function (b){
+//         return function (c){
+//             return function (d){
+//                 return a+b+c+d
+//             }
+//         }
+//     }
+// }
+//
+// console.log(carriyng(10)(20)(30)(40))
+//
+//
+// // => ()
+// // const task = () =>{}
+//
+//
+// // closure = замыкания
+//
+// let number = 7
+//
+// function task(n){
+//     return 10+n
+// }
+//
+// console.log(task(number))
+//
+// function closure(){
+//     return 10+number
+// }
+//
+// console.log(closure())
+
+
+// recursion - рекурсия
+
+// let n = 0
+// function funk (){
+//     n++
+//     console.log(n)
+//     // return funk()
+//     return n>10?n:funk()
+// }
+//
+// console.log(funk())
+
+
+
+// let n = 0
+//
+// function recursion(){
+//     n++
+//     console.log(n)
+//     // return recursion()
+//     return n>10?n:recursion()
+// }
+
+// if > :
+// else > ?
+
+// console.log(recursion())
+
+
+// factorial
+
+// 5!5x4x3x2x1
+
+function factorial(n){
+
+}
+
+console.log(factorial(6))
+console.log(factorial(3))
